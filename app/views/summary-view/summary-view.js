6f1b7dc6d4e86fa66247a41e82af34e4b237a54d
Yael Serry <yaels@codevalue.net>
2018-01-24 16:50:24 +0200

    platform specific styles, http call

--- app/views/summary-view/summary-view.js
+++ app/views/summary-view/summary-view.js
@@ -7,12 +7,23 @@ exports.loaded = function (args) {
             var ip = response.ip;
             const page = args.object;
             page.bindingContext = response;
+            response.country_name === "" ? response.country_name = undefined : response.country_name = response.country_name;
+            response.region_name === "" ? response.region_name = undefined : response.region_name = response.region_name;
+            response.city === "" ? response.city = undefined : response.city = response.city;
         }, function (error) {
             console.log(error);
             page.bindingContext = {
-                city: "",
-                region_name: "your home.",
-                country_name: ""
+                city: undefined,
+                region_name: "your location.",
+                country_name: undefined
             }
         });
+
+    const page = args.object;
+    const stackLayout = page.getViewById("thank-you-label-wrapper");
+    stackLayout.opacity = 0;
+    stackLayout.animate({
+        opacity: 1,
+        duration: 1000
+    })
 }
